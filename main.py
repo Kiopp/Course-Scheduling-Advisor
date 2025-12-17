e98d568d5f054122ea94cfbf1a82270203fda3ef
Jesper Wentzell <jesper.wentzell@gmail.com>
2025-12-17 12:35:02 +0100

    Transformed translate.py to a function and improved the menu system

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-def selectInstance():
+from translate import translate_facts_from_kg
+
+# Initial value
+selected_file = ""
+
+#
+def select_instance():
     print("--- Welcome to the course scheduler! ---")
-    print("1. Instance 1: JU")
-    print("2. Instance 2: KTH")
+    print("1. From knowledge graph")
+    print("2. Test case 1")
 
     choice = input("Enter your choice (1-2): ")
 
     if choice == "1":
-        print("Selecting JU instance...")
+        print("Selecting from kg...")
+        translate_facts_from_kg()
+        return "facts.lp"
     elif choice == "2":
-        print("Selecting KTH instance...")
+        print("Selecting test case 1...")
+        return "test.lp"
     else:
-        print("You stupid mf.")
+        return ""
 
-selectInstance()
+# Keep going if input is invalid
+while selected_file == "":
+    selected_file = select_instance()
+    if selected_file == "":
+        print("Invalid input, please try again...\n")
+print(selected_file)
